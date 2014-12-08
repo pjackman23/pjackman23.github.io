a46233868a6af4f928a8cf05398ee016c4a35298
pjackman <pjackman23@gmail.com>
2014-12-07 21:09:20 -0600

    Mobile screen
    Created construction screen for mobile site.

--- js/main.js
+++ js/main.js
@@ -21,6 +21,18 @@ var Body = {
 
 		$(".exp-p").css('min-width', parseInt(screen.width * 0.35) + "px");
 		$(".exp-a").css('min-width', parseInt(screen.width * 0.35) + "px");
+	},
+
+	detectMobileDevice: function() { 
+		if( navigator.userAgent.match(/Android/i)
+		 || navigator.userAgent.match(/webOS/i)
+		 || navigator.userAgent.match(/iPhone/i)
+		 || navigator.userAgent.match(/iPad/i)
+		 || navigator.userAgent.match(/iPod/i)
+		 || navigator.userAgent.match(/BlackBerry/i)
+		 || navigator.userAgent.match(/Windows Phone/i) ) {
+			$("#mobile-overlay").css('visibility', "visible");	  
+		}
 	}
 }
 
